1f469f93aa998d4c3488487d38947d765eafc9dd
Thinh Nguyen <thinh.nguyen@dirox.net>
2025-09-09 22:26:19 +0700

    feat(config): server port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
-    '@nuxt/ui'
+    '@nuxt/ui',
   ],
+  // Dev server config: https://nuxt.com/docs/4.x/api/nuxt-config#devserver
+  devServer: {
+    // https://nuxt.com/docs/4.x/api/nuxt-config#port
+    port: Number(process.env.APP_PORT || 3000),
+  },
 })
